517cad6170bbded320958240c738263ebf9eb4cd
Vitor Mouzinho <vitormouzinho@Vitors-MacBook-Pro.local>
2024-05-27 20:39:24 -0300

    feat: start adding streamlit

--- src/main.py
+++ src/main.py
@@ -8,10 +8,15 @@ import pandas as pd
 from snowcleaner.model.pair_generator import calculate_similarity_scores_for_pairs, block_records_with_threshold
 from snowcleaner.model.train_model import train_model, predict
 from snowcleaner.clusteridgenerator.clusteridgenerator import generate_cluster_ids
+import streamlit as st
+
+st.set_page_config(page_title="SnowCleaner",layout="wide")
+st.subheader("Welcome to SnowCleaner")
 
 features = ["first_name", "last_name", "address", "city", "state", "country"]
 
 df = pd.read_csv('exampledatasets/data.csv')
+st.dataframe(df, use_container_width=True)
 
 # Accumulate similarity scores
 similarity_scores = []
@@ -48,19 +53,28 @@ training_data = results.drop(columns=['pk','pk_2'])
 
 model = train_model(training_data)
 final_results = predict(model,result_df.drop(columns="pair"))
+
 df1 = generate_cluster_ids(final_results)
 df1 = df1[df1['group_id'] != '']
 
 
-merged_df_pk = pd.merge(df, df1, left_on='pk', right_on='pk_2', how='left')
+pk1 = df1[["pk","group_id"]]
+pk2 = df1[["pk_2","group_id"]]
 
-# Merge based on 'pk_2'
-merged_df_pk_2 = pd.merge(df, df1, left_on='pk', right_on='pk', how='left')
+pk1 = pk1.drop_duplicates(subset=['pk'])
+pk2 = pk2.drop_duplicates(subset=['pk_2'])
 
-# Combine the results
+merged_df_pk_2 = pd.merge(df, pk1, left_on='pk', right_on='pk', how='left')
+
+merged_df_pk = pd.merge(df, pk2, left_on='pk', right_on='pk_2', how='left')
+
+
 merged_df = merged_df_pk.combine_first(merged_df_pk_2)
 
+
 # Drop redundant columns
-merged_df = merged_df[features + ["group_id","pk"]]
+merged_df = merged_df[["pk","group_id"] + features]
 merged_df.to_csv("results.csv")
+
+st.dataframe(merged_df, use_container_width=True)
 
